42f69264e1446b35fe59945097d67005dd5d62d8
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-27 19:58:00 -0500

    [NEW] - Create API REST - Method for CRUD from Hotel

---
+++ Hotel_api/Hotel_api/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Hotel_api.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category_hotel { get; set; }
+        public int Price { get; set; }
+        public int Client_calif { get; set; }
+    }
+}
